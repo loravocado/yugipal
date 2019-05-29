b6b93cecbfd0587e97f08a04986e4f3e5a25f214
Lora <lora_ma@hotmail.ca>
2019-05-28 22:39:59 -0600

    All title buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import {Button} from 'react-bootstrap';
+import './style/App-Style.css'
+import {Image, Container, Row, Col} from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-    </div>
-  );
+    <div>
+      <div>
+        <Image id='logo' src="https://imgur.com/BA36iPY.png" responsive />
+        <Button id="button1" variant="outline-dark" size='lg'>Discover</Button>
+        <Button id="button1" variant="outline-dark" size='lg'>Collection</Button>
+        <Button id="button1" variant="outline-dark" size='lg'>Dashboard</Button>
+        <Button id="button1" variant="outline-dark" size='lg'> +Add Card </Button>
+        <Button id="button2" variant="outline-dark" size='sm'>login</Button>
+        <Button id="button3" variant="outline-dark" size='sm'>signup</Button>
+      </div>
+      <hr id="hor-line"></hr>
+  </div>
+);
 }
 
 export default App;
