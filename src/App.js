0144dc5936389fa171111c19285d8b439d3b12b2
Lora <lora_ma@hotmail.ca>
2019-05-29 00:36:46 -0600

    search bar added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import {Button} from 'react-bootstrap';
 import './style/App-Style.css'
 import {Image, Container, Row, Col} from 'react-bootstrap';
+import {Form, FormControl} from 'react-bootstrap';
 
 function App() {
   return (
     <div>
-      <div>
+      <div> {/* Top buttons*/}
         <Image id='logo' src="https://imgur.com/BA36iPY.png" responsive />
         <Button id="button1" variant="outline-dark" size='lg'>Discover</Button>
         <Button id="button1" variant="outline-dark" size='lg'>Collection</Button>
@@ -14,8 +15,26 @@ function App() {
         <Button id="button1" variant="outline-dark" size='lg'> +Add Card </Button>
         <Button id="button2" variant="outline-dark" size='sm'>login</Button>
         <Button id="button3" variant="outline-dark" size='sm'>signup</Button>
+        <hr id="hor-line"></hr>
       </div>
-      <hr id="hor-line"></hr>
+      <div>
+      <h2 id= 'title'>Stevies Yugioh Cards </h2> {/* Replace Stevies with name*/}
+      </div>
+
+      <div>
+        <Image id='subheading1' src="https://imgur.com/muF0iB0.png" responsive />
+        <p id='inlineText'>432</p> {/* Replace 432 with card number*/}
+        <Image id='subheading2' src="https://imgur.com/fleHPc6.png" responsive />
+        <p id='inlineText'>3001.45</p> {/* Replace 3001.45 with worth of cards*/}
+      </div>
+
+      <div>
+        <Form inline>
+          <Image id='searchIcon' src="https://imgur.com/vKWXWwn.png" responsive />
+          <FormControl id='searchBar' type="text" placeholder="Search Stevies Cards" className="mr-sm-2" />
+        </Form>
+    </div>
+
   </div>
 );
 }
